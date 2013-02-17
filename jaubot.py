dd65f90f76ff590aee0edb20619c340d0d06c204
Juha Autero <jautero@iki.fi>
2013-02-17 22:49:28 +0200

    Removed non-working/unnecessary modules

--- jaubot.py
+++ jaubot.py
@@ -1,7 +1,8 @@
 #!/usr/local/bin/python2.4
 # Ircbot for pinging.
 
-import ircbot, time, urlbot3, pingbot, os, sys, reminderbot, replybot, quotebot, evalbot, pinky, delistore
+import ircbot, time, urlbot3, pingbot, os, sys, reminderbot, replybot
+# quotebot, evalbot, pinky, delistore
 
 debuglog=open("debug.log","w")
 
@@ -35,27 +36,27 @@ def leave(m, origin, args, text, bot=bot):
 bot.rule(invite,'invite',cmd="INVITE")
 bot.rule(leave,'leave',cmd="KICK")
 
-urlstore=delistore.DeliciousUrlstore()
+#urlstore=delistore.DeliciousUrlstore()
 
-urlbot3.init_bot(bot,urlstore)
+urlbot3.init_bot(bot)
 pingbot.init_bot(bot)
 reminderbot.init_bot(bot)
-pinky.init_bot(bot)
+#pinky.init_bot(bot)
 replybot.init_bot(bot)
-quotebot.init_bot(bot)
-evalbot.init_bot(bot)
+#quotebot.init_bot(bot)
+#evalbot.init_bot(bot)
 
 ircbot.doubleFork()
 
 bot.run(server[0],server[1])
 
-evalbot.uninit_bot(bot)
-quotebot.uninit_bot(bot)
+#evalbot.uninit_bot(bot)
+#quotebot.uninit_bot(bot)
 urlbot3.uninit_bot(bot)
 pingbot.uninit_bot(bot)
 reminderbot.uninit_bot(bot)
 replybot.uninit_bot(bot)
-pinky.uninit_bot(bot)
+#pinky.uninit_bot(bot)
 
 #if bot.restart:
 #    os.execlp(sys.argv[0],sys.argv)
